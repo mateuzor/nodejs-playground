4b6dd3f2b8f8f2089d105ca93ca359bbdf468b0f
Mateus Ramos Ferreira <mateuzor@Mateuss-MacBook-Pro.local>
2023-02-02 09:37:01 -0300

    storing audio

--- express-backend/index.js
+++ express-backend/index.js
@@ -1,10 +1,38 @@
 const express = require("express");
 const app = express();
 const port = 3000;
+const cors = require("cors");
+const multer = require("multer");
+
+// const uploadDest = "public/media/";
+// const allowedMimeTypes = ["audio/mp3"];
+// const filter = function (req, file, cb) {
+//   if (!allowedMimeTypes.includes(file.mimetype.toLowerCase())) {
+//     cb(null, false);
+//   }
+//   cb(null, true);
+// };
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "uploads/audio/");
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.originalname);
+  },
+});
+
+const upload = multer({ dest: os.tmpdir() });
+
+app.use(cors());
 
 app.get("/", (req, res) => {
   res.send("Hello World, from express");
 });
+
+// app.post("/upload-audio", multer({ storage }).single("audio"), (req, res) => {
+//   res.send("Audio file successfully uploaded!");
+// });
 
 app.listen(port, () =>
   console.log(`Hello world app listening on port ${port}!`)

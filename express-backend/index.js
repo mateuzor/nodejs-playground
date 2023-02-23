473264d4a2f92843a1ade3bbfe451007a5db0035
Mateus Ramos Ferreira <mateuzor@Mateuss-MacBook-Pro.local>
2023-02-23 09:27:35 -0300

    reproducing song from static url through my enpoint

--- express-backend/index.js
+++ express-backend/index.js
@@ -1,17 +1,13 @@
 const express = require("express");
 const app = express();
-const port = 3000;
+const port = 3001;
+const axios = require("axios");
 const cors = require("cors");
 const multer = require("multer");
+const fs = require("fs");
 
-// const uploadDest = "public/media/";
-// const allowedMimeTypes = ["audio/mp3"];
-// const filter = function (req, file, cb) {
-//   if (!allowedMimeTypes.includes(file.mimetype.toLowerCase())) {
-//     cb(null, false);
-//   }
-//   cb(null, true);
-// };
+const mp3Url =
+  "http://commondatastorage.googleapis.com/codeskulptor-demos/DDR_assets/Sevish_-__nbsp_.mp3";
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
@@ -22,15 +18,56 @@ const storage = multer.diskStorage({
   },
 });
 
+const upload = multer({ storage });
+
 app.use(cors());
 
 app.get("/", (req, res) => {
-  res.send("Hello World, from express");
+  res.send("Hello World");
 });
 
-// app.post("/upload-audio", multer({ storage }).single("audio"), (req, res) => {
-//   res.send("Audio file successfully uploaded!");
-// });
+app.get("/play-local", (req, res) => {
+  fs.readFile("./uploads/audio/music.mp3", function (err, result) {
+    res.send(result.toString("base64"));
+    return result;
+  });
+});
+
+app.get("/playlist", (req, res) => {
+  axios
+    .get(mp3Url, {
+      responseType: "stream",
+      "Content-Range": "bytes 16561-8065611",
+    })
+    .then((Response) => {
+      const stream = Response.data;
+
+      res.set("content-type", "audio/mp3");
+      res.set("accept-ranges", "bytes");
+      res.set("content-length", Response.headers["content-length"]);
+      console.log(Response);
+
+      stream.on("data", (chunk) => {
+        console.log(chunk);
+        res.write(chunk);
+      });
+
+      stream.on("error", (err) => {
+        res.sendStatus(404);
+      });
+
+      stream.on("end", () => {
+        res.end();
+      });
+    })
+    .catch((Err) => {
+      console.log(Err.message);
+    });
+});
+
+app.post("/upload-audio", multer({ storage }).single("audio"), (req, res) => {
+  res.send("Audio file successfully uploaded!");
+});
 
 app.listen(port, () =>
   console.log(`Hello world app listening on port ${port}!`)

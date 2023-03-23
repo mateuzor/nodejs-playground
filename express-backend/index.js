ca9eb8ae34eda2eeb7326e1008f3789250be3ccf
Mateus Ramos Ferreira <mateuzor@Mateuss-MacBook-Pro.local>
2023-03-23 08:12:31 -0300

    playing song from local folder

--- express-backend/index.js
+++ express-backend/index.js
@@ -1,72 +1,112 @@
-const express = require("express");
+import fs from "fs-extra";
+import express from "express";
+import crypto from "crypto";
+import chalk from "chalk";
+import cors from "cors";
 const app = express();
-const port = 3001;
-const axios = require("axios");
-const cors = require("cors");
-const multer = require("multer");
-const fs = require("fs");
+const port = 4001;
 
-const mp3Url =
-  "http://commondatastorage.googleapis.com/codeskulptor-demos/DDR_assets/Sevish_-__nbsp_.mp3";
+import musics from "./data.json" assert { type: "json" };
 
-const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, "uploads/audio/");
-  },
-  filename: function (req, file, cb) {
-    cb(null, file.originalname);
-  },
-});
+import CliProgress from "cli-progress";
 
-const upload = multer({ storage });
+const mp3DirPath = "./mp3";
+const progress = new CliProgress.Bar({}, CliProgress.Presets.shades_classic);
 
 app.use(cors());
 
 app.get("/", (req, res) => {
-  res.send("Hello World");
-});
+  async function readDir() {
+    try {
+      const files = await fs.readdir(mp3DirPath);
+      console.log(chalk.blue(" Files have been read \n"));
+      return files;
+    } catch (err) {
+      console.error(chalk.red(err));
+    }
+  }
 
-app.get("/play-local", (req, res) => {
-  fs.readFile("./uploads/audio/music.mp3", function (err, result) {
-    res.send(result.toString("base64"));
-    return result;
+  readDir().then((files) => {
+    progress.start(files.length, 0);
+    const output = files.map((file, i) => {
+      const stats = fs.statSync(`${mp3DirPath}/${file}`);
+      const songInfo = file.split("-");
+      return {
+        id: crypto.randomUUID(),
+        song: songInfo[1].replace(".mp3", ""),
+        artist: songInfo[0].trim(),
+        src: `${mp3DirPath}/${file}`,
+        image:
+          "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSg83frs-hZlNhu-CJmQNXtad8EbmZp7HztMg&usqp=CAU",
+        fileSize: stats.size,
+      };
+    });
+    fs.writeFile("data.json", JSON.stringify(output), "utf8", (err) => {
+      if (err) {
+        console.error(chalk.red(err));
+        throw err;
+      }
+      progress.stop();
+      console.log(
+        chalk.green("\n Process run complete"),
+        chalk.blue("\n\n Output saved to ./data.json\n")
+      );
+      return null;
+    });
   });
 });
 
 app.get("/playlist", (req, res) => {
-  axios
-    .get(mp3Url, {
-      responseType: "stream",
-      "Content-Range": "bytes 16561-8065611",
-    })
-    .then((Response) => {
-      const stream = Response.data;
-
-      res.set("content-type", "audio/mp3");
-      res.set("accept-ranges", "bytes");
-      res.set("content-length", Response.headers["content-length"]);
-      console.log(Response);
-
-      stream.on("data", (chunk) => {
-        console.log(chunk);
-        res.write(chunk);
-      });
-
-      stream.on("error", (err) => {
-        res.sendStatus(404);
-      });
-
-      stream.on("end", () => {
-        res.end();
-      });
-    })
-    .catch((Err) => {
-      console.log(Err.message);
-    });
+  res.send(musics);
 });
 
-app.post("/upload-audio", multer({ storage }).single("audio"), (req, res) => {
-  res.send("Audio file successfully uploaded!");
+app.get("/song/:id", function (req, res) {
+  var i;
+
+  var musicPath;
+
+  for (i = 0; i < musics.length; ++i) {
+    if (musics[i].id == req.params.id) {
+      musicPath = musics[i].src;
+      var range;
+      var stat = fs.statSync(musicPath);
+      range = req.headers.range;
+      var readStream;
+
+      if (range !== undefined) {
+        var parts = range.replace(/bytes=/, "").split("-");
+
+        var partial_start = parts[0];
+        var partial_end = parts[1];
+
+        if (
+          (isNaN(partial_start) && partial_start.length > 1) ||
+          (isNaN(partial_end) && partial_end.length > 1)
+        ) {
+          return res.sendStatus(500);
+        }
+
+        var start = parseInt(partial_start, 10);
+        var end = partial_end ? parseInt(partial_end, 10) : stat.size - 1;
+        var content_length = end - start + 1;
+
+        res.status(206).header({
+          "Content-Type": "audio/mpeg",
+          "Content-Length": content_length,
+          "Content-Range": "bytes " + start + "-" + end + "/" + stat.size,
+        });
+
+        readStream = fs.createReadStream(musicPath, { start: start, end: end });
+      } else {
+        res.header({
+          "Content-Type": "audio/mpeg",
+          "Content-Length": stat.size,
+        });
+        readStream = fs.createReadStream(musicPath);
+      }
+      readStream.pipe(res);
+    }
+  }
 });
 
 app.listen(port, () =>
